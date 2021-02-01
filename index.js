102ec1e9615af022b58506b5ce6de857c6febf2d
Gavin Sharkey <gavinsharkey2016@gmail.com>
2021-02-01 04:06:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,26 +4,28 @@ function findMinAndRemoveSorted(array) {
 
 function merge(leftArray, rightArray) {
   const sorted = [];
-  
+
   while (leftArray.length !== 0 && rightArray.length !== 0) {
     if (leftArray[0] < rightArray[0]) {
-      sorted.push(findMinAndRemoveSorted(leftArray));
+      sorted.push(leftArray[0]);
+      leftArray = leftArray.slice(1)
     } else {
-      sorted.push(findMinAndRemoveSorted(rightArray));
+      sorted.push(rightArray[0]);
+      rightArray = rightArray.slice(1)
     }
   }
-  
-  return sorted.concat(leftArray).concat(rightArray);
+
+  return sorted.concat(leftArray).concat(rightArray)
 }
 
 function mergeSort(array) {
-  const midpoint = array.length / 2;
-  const leftArray = array.slice(0, midpoint);
-  const rightArray = array.slice(midpoint, array.length);
-  
-  if (array.length === 0) {
+  const midpoint = array.length / 2
+  const leftHalf = array.slice(0, midpoint)
+  const rightHalf = array.slice(midpoint, array.length)
+
+  if (array.length === 1) {
     return array;
   } else {
-    return merge(mergeSort(leftArray), mergeSort(rightArray))
+    return merge(mergeSort(leftHalf), mergeSort(rightHalf))
   }
 }

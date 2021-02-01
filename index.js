1ee3d2b3717770d71bc0b8bce62fcf68eac22521
Gavin Sharkey <gavinsharkey2016@gmail.com>
2021-02-01 04:02:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,5 +17,13 @@ function merge(leftArray, rightArray) {
 }
 
 function mergeSort(array) {
+  const midpoint = array.length / 2;
+  const leftArray = array.slice(0, midpoint);
+  const rightArray = array.slice(midpoint, array.length);
   
+  if (array.length === 0) {
+    return array;
+  } else {
+    return merge(mergeSort(leftArray), mergeSort(rightArray))
+  }
 }
